a263f7ec26e3e999737b29507c95b1202e67bdcc
Dmitrii <testingshaine@gmail.com>
2019-04-25 11:26:00 +0300

    Lesson4

---
+++ lesson 3 dop/script.js
@@ -0,0 +1,6 @@
+
+// let str = "урок-3-был слишком легким";
+// console.log(str[0].toUpperCase() + str.slice(1));
+let str = "урок-3-был слишком легким";
+console.log(str.replace(/-/g, " "));
+
---
+++ lesson 4/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+for (let key in appData) {
+  console.log("Наша программа включает в себя данные:" + appData[key]);
+}
